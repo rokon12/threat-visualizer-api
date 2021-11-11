1ea6cb5da4153007932a4efb497b78e34ed34be4
A N M Bazlur Rahman <anmbrr.bit0112@gmail.com>
2021-11-10 20:53:57 -0500

    Initial commit

---
+++ src/main/java/com/example/stv/ThreatVisualizedApplication.java
@@ -0,0 +1,13 @@
+package com.example.stv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreatVisualizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreatVisualizedApplication.class, args);
+	}
+
+}
