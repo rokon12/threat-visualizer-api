e1cf96be0701d93b4f25ab6d1cb5e6f7f2d4b411
A N M Bazlur Rahman <anmbrr.bit0112@gmail.com>
2021-11-10 21:12:24 -0500

    Fix github pipeline

--- src/main/java/ca/bazlur/visualizer/ThreatVisualizedApplication.java
+++ src/main/java/ca/bazlur/visualizer/ThreatVisualizedApplication.java
@@ -1,4 +1,4 @@
-package com.example.stv;
+package ca.bazlur.visualizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ca/bazlur/visualizer/ThreatVisualizedApplicationTests.java
+++ src/test/java/ca/bazlur/visualizer/ThreatVisualizedApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.stv;
+package ca.bazlur.visualizer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SecurityThreatVisualizedApplicationTests {
+class ThreatVisualizedApplicationTests {
 
 	@Test
 	void contextLoads() {
