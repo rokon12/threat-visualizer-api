a1e97e9e712ba54f58753c3ef7abc99077d003be
A N M Bazlur Rahman <anmbrr.bit0112@gmail.com>
2021-11-17 21:51:02 -0500

    Update images

--- src/test/java/ca/bazlur/visualizer/ThreatVisualizedApplicationTests.java
+++ src/test/java/ca/bazlur/visualizer/ThreatVisualizedApplicationTests.java
@@ -1,15 +1,22 @@
 package ca.bazlur.visualizer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.TestPropertySource;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 class ThreatVisualizedApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext, "applicationContext should not null!");
     }
-
 }
